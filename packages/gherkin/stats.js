ecd2a51224f8f481aafe7fdc5b531f447bef8d5c
Sebastian Schürmann <sebs@2xs.org>
2018-10-04 21:01:20 +0200

    refactor(gherkin): refactored stats to be pre Chrome 69 compatible

--- packages/gherkin/stats.js
+++ packages/gherkin/stats.js
@@ -2,20 +2,29 @@ export const stats = (parsedFeature)=>{
 	
 	const features = parsedFeature.length;
 	const add = (a, b) => a + b;
-    
+	
+	function flatten(arr) {
+		return arr.reduce(function (flat, toFlatten) {
+			return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
+		}, []);
+	}
+	
 	const scenarios = parsedFeature.map(feature => feature.scenarios.length).reduce(add);
 
-	const given = parsedFeature
-									.map(feature => feature.scenarios.map(scenario => scenario.given)
-									.flat()).flat().length;
+	var given = parsedFeature
+									.map(feature => feature.scenarios.map(scenario => scenario.given));
 
-	const when = parsedFeature
-				.map(feature => feature.scenarios.map(scenario => scenario.when)
-				.flat()).flat().length;
-			
-	const then = parsedFeature
-				.map(feature => feature.scenarios.map(scenario => scenario.then)
-				.flat()).flat().length;
+	given = flatten(given).length;	
+	
+	var when = parsedFeature
+									.map(feature => feature.scenarios.map(scenario => scenario.when));
+
+	when = flatten(when).length;	
+	
+	var then = parsedFeature
+									.map(feature => feature.scenarios.map(scenario => scenario.then));
+
+	then = flatten(then).length;	
 
 	const score = features + scenarios + given + when + then;
 
--- packages/gherkin/test/stats.js
+++ packages/gherkin/test/stats.js
@@ -28,4 +28,29 @@ Assert.equal(statistics.given, 2);
 Assert.equal(statistics.when, 2);
 Assert.equal(statistics.then, 3);
 
+const otherFeature = `
 
+Feature: Can drink beer when thirsty
+As a drinker
+I want to take beer off the wall
+In order to satisfy my thirst
+
+Scenario: Can take a single beer
+  Given 100 bottles of beer on the wall
+  When a bottle is taken down
+  Then there are 99 bottles of beer on the wall
+  And Then foo
+
+Scenario: Bar
+  Given 200 Bottles are sold
+  When the Barkeeper deilvers them 
+	Then the Baarkeeper is tired
+	`;
+let otherParsed = parse(otherFeature);
+let otherStats = stats(otherParsed);
+console.log(otherStats);
+Assert.equal(otherStats.features, 1);
+Assert.equal(otherStats.scenarios, 2);
+Assert.equal(otherStats.given, 2);
+Assert.equal(otherStats.when, 2);
+Assert.equal(otherStats.then, 3);
