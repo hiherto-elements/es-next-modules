177d141fde13ae3d079e04f588e186fea40aec39
Sebastian Schürmann <sebs@2xs.org>
2018-10-10 18:06:50 +0200

    feature(diff); diff json and a line

--- packages/diff/test.sh
+++ packages/diff/test.sh
@@ -2,3 +2,4 @@
 ~/.deno/bin/deno test/array.js
 ~/.deno/bin/deno test/character.js
 ~/.deno/bin/deno test/css.js
+~/.deno/bin/deno test/json.js
---
+++ packages/diff/test/json.js
@@ -0,0 +1,4 @@
+import { Assert } from '../node_modules/@hiherto-elements/assert/Assert.js';
+import { diffJson } from '../json.js';
+
+// Assert.isFunction(diffJson)
