7c0503c6b9917aec42b1ab0b94218c195de24a61
Sebastian Schürmann <sebs@2xs.org>
2018-10-10 22:05:58 +0200

    build(all): Add site generator

---
+++ site.sh
@@ -0,0 +1,9 @@
+rm -rf site/*
+cp -r ./packages/assert/docs site/assert
+cp -r ./packages/diff/docs site/diff
+cp -r ./packages/esi/docs site/esi
+cp -r ./packages/gherkin/docs site/gherkin
+cp -r ./packages/natural/docs site/natural
+cp -r ./packages/punycode/docs site/punycode
+cp -r ./packages/querystring/docs site/querystring
+cp -r ./packages/url/docs site/url
