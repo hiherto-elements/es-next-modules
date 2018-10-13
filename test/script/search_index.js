b878f74315806e68aafeca4470c0761705582caf
Sebastian Schürmann <sebs@2xs.org>
2018-10-13 03:39:43 +0200

    Updates

---
+++ test/script/search_index.js
@@ -0,0 +1,62 @@
+window.esdocSearchIndex = [
+  [
+    "@hiherto-elements/test/test/tdd.js~plan",
+    "function/index.html#static-function-plan",
+    "<span>plan</span> <span class=\"search-result-import-path\">@hiherto-elements/test/test/tdd.js</span>",
+    "function"
+  ],
+  [
+    "@hiherto-elements/test/test/tdd.js~stats",
+    "function/index.html#static-function-stats",
+    "<span>stats</span> <span class=\"search-result-import-path\">@hiherto-elements/test/test/tdd.js</span>",
+    "function"
+  ],
+  [
+    "@hiherto-elements/test/test/tdd.js~suite",
+    "function/index.html#static-function-suite",
+    "<span>suite</span> <span class=\"search-result-import-path\">@hiherto-elements/test/test/tdd.js</span>",
+    "function"
+  ],
+  [
+    "@hiherto-elements/test/test/tdd.js~test",
+    "function/index.html#static-function-test",
+    "<span>test</span> <span class=\"search-result-import-path\">@hiherto-elements/test/test/tdd.js</span>",
+    "function"
+  ],
+  [
+    "@hiherto-elements/test/test/test.js~test",
+    "variable/index.html#static-variable-test",
+    "<span>test</span> <span class=\"search-result-import-path\">@hiherto-elements/test/test/test.js</span>",
+    "variable"
+  ],
+  [
+    "test/node_modules/@hiherto-elements/assert/test/test.js",
+    "file/test/node_modules/@hiherto-elements/assert/test/test.js.html",
+    "test/node_modules/@hiherto-elements/assert/test/test.js",
+    "file"
+  ],
+  [
+    "test/tdd.js",
+    "file/test/tdd.js.html",
+    "test/tdd.js",
+    "file"
+  ],
+  [
+    "test/test.js",
+    "file/test/test.js.html",
+    "test/test.js",
+    "file"
+  ],
+  [
+    "test/test/tdd.js",
+    "file/test/test/tdd.js.html",
+    "test/test/tdd.js",
+    "file"
+  ],
+  [
+    "test/test/test.js",
+    "file/test/test/test.js.html",
+    "test/test/test.js",
+    "file"
+  ]
+]
