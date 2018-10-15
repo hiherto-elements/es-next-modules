3444079dc2adea7ecd1a090ea7ea13731134dc46
Sebastian Schürmann <sebs@2xs.org>
2018-10-16 00:28:10 +0200

    fix(natural): tokenizer

--- packages/natural/test/tokenizers/regexp-tokenizer.js
+++ packages/natural/test/tokenizers/regexp-tokenizer.js
@@ -1,3 +1,8 @@
 import { Assert } from '../../node_modules/@hiherto-elements/assert/Assert.js';
 import { RegexpTokenizer } from '../../src/tokenizers/regexp-tokenizer.js';
 Assert.isFunction(RegexpTokenizer);
+const res = new RegexpTokenizer({
+    pattern: /^The/
+}).tokenize('The quick brown');
+console.log(res);
+
