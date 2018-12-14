f19dfd0d71d8c7a0e683175063ab1ba9d7ab95dd
Sebastian Schürmann <sebs@2xs.org>
2018-12-14 22:03:26 +0100

    feature(lame): port more code to es6

--- packages/lame/test.sh
+++ packages/lame/test.sh
@@ -8,8 +8,10 @@
 ~/.deno/bin/deno test/III_psy_xmin.js
 ~/.deno/bin/deno test/III_psy_ratio.js
 ~/.deno/bin/deno test/IIISideInfo.js
+~/.deno/bin/deno test/LameInternalFlags.js
 ~/.deno/bin/deno test/NewMDCT.js
 ~/.deno/bin/deno test/NsPsy.js
 ~/.deno/bin/deno test/ScaleFac.js
 ~/.deno/bin/deno test/Tables.js
+~/.deno/bin/deno test/VBRSeekInfo.js
 ~/.deno/bin/deno test/Version.js
---
+++ packages/lame/test/LameInternalFlags.js
@@ -0,0 +1,3 @@
+import { Assert } from '../node_modules/@hiherto-elements/assert/Assert.js';
+import { LameInternalFlags } from '../LameInternalFlags.js';
+Assert.isFunction(LameInternalFlags)
